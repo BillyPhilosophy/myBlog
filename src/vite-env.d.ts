fa1382a70958a1e9af560a207b590285c4a2b020
BillyPhilosophy <d497717947@gmail.com>
2022-08-05 17:44:28 +0800

    初始化配置自动导入组件和自动导入api，包含第三方api和vue的组合式api和vue router

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,5 +1,5 @@
 /// <reference types="vite/client" />
-
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/ban-types
 declare module '*.vue' {
   import type { DefineComponent } from 'vue'
   const component: DefineComponent<{}, {}, any>
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,29 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [
+    vue(),
+    AutoImport({
+      dts: true,
+      imports: ['vue', 'vue-router'],
+      resolvers: [ElementPlusResolver()],
+    }),
+    Components({
+      dirs: ['src/components'],
+      extensions: ['vue'],
+      dts: true,
+      resolvers: [ElementPlusResolver()],
+    }),
+  ],
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src')
+    }
+  },
 })
