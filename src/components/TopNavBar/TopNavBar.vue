46b17771ab1660c0eedb99a6d441fb477fd03f79
BillyPhilosophy <d497717947@gmail.com>
2023-12-05 17:01:05 +0800

    feat topnavbar添加用户模块

--- src/components/TopNavBar/TopNavBar.vue
+++ src/components/TopNavBar/TopNavBar.vue
@@ -2,14 +2,17 @@
   <nav class="navbar">
     <el-header>
       <el-row :gutter="20">
+        <!-- logo -->
         <el-col :span="4">
           <Logo @openPollup="isShow = true" />
         </el-col>
+        <!-- titlebar -->
         <el-col :span="16">
           <Nav />
         </el-col>
+        <!-- userinfo -->
         <el-col :span="4">
-
+          <RightInfoBox :userinfo="userInfo" @logout="logout" />
         </el-col>
       </el-row>
     </el-header>
@@ -17,27 +20,33 @@
 </template>
 
 <script setup lang="ts">
-import { isRef } from 'vue'
+import { removeItem } from '@/utils/storage';
 import { useStore } from '@/store/main';
 import { useUserStore } from '@/store/user';
 import { getBaseUserInfo } from "@/api/user";
 const store = useStore();
 const userStore = useUserStore();
 const { user } = store;
-const { userInfo,setBaseInfo } = userStore;
-// console.log(isRef(userInfo));
+const { userInfo, setBaseInfo } = userStore;
 const isShow = ref(false);
 const queryBaseUserInfo = () => {
   if (user.token) {
     getBaseUserInfo().then((res: any) => {
       const { data } = res;
-      if(data.returnCode==0){
+      if (data.returnCode == 0) {
         setBaseInfo(data.body)
+      } else {
+        ElMessage.error(data.msg)
+        removeItem('user')
       }
     })
   }
 };
 queryBaseUserInfo();
+
+const logout = () =>{
+  
+}
 
 </script>
 
---
+++ src/components/TopNavBar/components/RightInfoBox.vue
@@ -0,0 +1,88 @@
+<template>
+  <aside class="right">
+    <ul v-if="userInfo.userNo" class="icon-group">
+      <li>
+        <i class="iconfont icon-mail"></i>
+      </li>
+      <li @click="router.push('/index/options')">
+        <i class="iconfont icon-shezhi"></i>
+      </li>
+      <li>
+        <i class="iconfont icon-changyonggongneng"></i>
+      </li>
+    </ul>
+    <p v-else>
+      访客你好 去<RouterLink to="/login" style="color: rgb(124, 12, 226)">登录</RouterLink>
+    </p>
+    <div class="container" @mouseenter="infoShow = true" @mouseleave="infoShow = false">
+      <div class="avatar">
+        <MyElimage v-if="userInfo.userNo" :img="userInfo.avatar" :zip="2" />
+        <MyElimage v-else :img="Logo" alt="占位图片" :zip="2" />
+      </div>
+      <transition name="toptodown" mode="out-in">
+        <div v-show="infoShow" class="info-box">
+          <!-- 显示个人用户设置的背景图 -->
+          <div class="imgmask" v-if="userInfo.banner"></div>
+          <div class="imgcontainer" v-if="userInfo.banner">
+            <MyElimage :img="userInfo.banner" :zip="3" v-if="userInfo.banner" />
+          </div>
+          <div class="loginbox" v-if="userInfo.userNo">
+            <div class="up">
+              <div class="info">
+                <h2>{{ userInfo.nickname }}</h2>
+                <div>{{ userInfo.introduce }}</div>
+              </div>
+            </div>
+            <div class="button">
+              <ul>
+                <li @click="router.push('/index/home')">
+                  <i class="iconfont icon-geren"></i>
+                  <span>个人中心</span>
+                </li>
+                <li @click="router.push('/index/create')">
+                  <i class="iconfont icon-edit"></i>
+                  <span>我的发布</span>
+                </li>
+                <li @click="router.push('/index/options')">
+                  <i class="iconfont icon-shezhi"></i>
+                  <span>设置</span>
+                </li>
+                <li @click="logout">
+                  <i class="iconfont icon-close_circle"></i>
+                  退出登录
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div class="nologin" v-else>
+            喵~ 点击这里跳转
+            <RouterLink to="/login" style="color: rgb(124, 12, 226)">登录 </RouterLink>
+          </div>
+        </div>
+      </transition>
+    </div>
+  </aside>
+</template>
+<script setup lang="ts">
+import { PropType } from 'vue'
+import { useRouter } from 'vue-router'
+import Logo from '@/assets/img/logo.png'
+defineProps({
+  userInfo: {
+    type: Object as PropType<UserEasy>,
+    default: () => {
+      return {}
+    }
+  }
+})
+const emit = defineEmits(['logout'])
+const router = useRouter()
+const logout = () => {
+  emit('logout')
+}
+let infoShow = ref(false)
+</script>
+
+<style lang="scss" scoped>
+@import '../style/rightInfoBox.scss';
+</style>
